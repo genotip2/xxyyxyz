8cfe9c2319126853098a0badae6dc815d844f594
genotip2 <genotip2@gmail.com>
2025-02-06 19:36:01 +0700

    Update scalping_signal.py

--- scalping_signal.py
+++ scalping_signal.py
@@ -265,7 +265,7 @@ def main():
             
             signal, price = generate_signal(pair, data)
             if signal:
-                send_telegram_alert(signal, pair, data['price'], data, price)
+                send_telegram_alert(signal, pair, data['close_price_m15'], data, price)
                 
             # Auto close position
             if pair in ACTIVE_BUYS:
